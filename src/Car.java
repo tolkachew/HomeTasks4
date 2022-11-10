12a026f596a76ab411edd6f4feac073ae9f83a13
Vladislav <tolkachew.wlad@yandex.ru>
2022-11-10 09:48:25 +0300

    s

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,115 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    private int id;
+    private String mark;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
+    private int registration_number;
+
+    public Car(){
+        this.id = 0;
+        this.mark = "";
+        this.model = "";
+        this.year = 0;
+        this.color = "";
+        this.price = 0;
+        this.registration_number = 0;
+    }
+
+    public Car(int id, String mark, String model, int year, String color, int price, int registration_number) {
+        this.id = id;
+        this.mark = mark;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.registration_number = registration_number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if(year>0) {
+            this.year = year;
+        } else System.out.println("Введенный год некорректный!");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if(price>0) {
+            this.price = price;
+        } else System.out.println("Введена некорректная цена!");
+    }
+
+    public int getRegistration_number() {
+        return registration_number;
+    }
+
+    public void setRegistration_number(int registration_number) {
+        this.registration_number = registration_number;
+    }
+
+    @Override
+    public String toString() {
+        return "Машина: " +
+                "id = " + id +
+                ", Марка = " + mark +
+                ", Модель = " + model +
+                ", Год выпуска = " + year +
+                ", Цвет = " + color  +
+                ", Цена = " + price +
+                ", Регистрационный номер = " + registration_number;
+    }
 }
+/*
+Создать классы, спецификации которых приведены ниже. Определить кон-
+структоры и методы set(), get(), toString(). Определить дополнительно
+методы в классе, создающем массив объектов. Задать критерий выбора данных
+и вывести эти данные на консоль. В каждом классе, обладающем информацией,
+должно быть объявлено несколько конструкторов.
+Car: id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер.
+Создать массив объектов. Вывести:
+a) список автомобилей заданной марки;
+b) список автомобилей заданной модели, которые эксплуатируются боль-
+ше n лет;
+c) список автомобилей заданного года выпуска, цена которых больше ука-
+занной.
+ */
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,106 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Запустить приложение? (yes/no)");
+        String n = sc.next();
+        switch (n) {
+            case "yes" -> {
+                Car[] cars_list = car();        //auto
+                // Car[] cars_list = cars_list();     //enter
+                System.out.println("Вывести список автомобилей заданной марки.");
+                System.out.print("Введите марку автомобиля: ");
+                markCheck(cars_list, sc.next());
+                System.out.println("Введите список автомобилей заданной модели, которые эксплуатируются больше n лет.");
+                System.out.print("Введите модель автомобиля: ");
+                String model = sc.next();
+                System.out.print("Введите количетво лет эксплуатации: ");
+                modelCheck(cars_list, model, sc.nextInt());
+                System.out.println("Вывести список автомобилей заданного года выпуска, цена которых больше указанной.");
+                System.out.print("Введите год выпуска: ");
+                int year = sc.nextInt();
+                System.out.print("Введите цену: ");
+                yearCheck(cars_list, year, sc.nextInt());
+            }
+            case "no" -> System.exit(0);
+            default -> System.out.println("Введите 'yes' или 'no'");
+        }
+    }
+
+    static Car[] car() {
+        Car[] cars = new Car[6];
+        cars[0] = new Car(1, "Hyundai", "Creta", 2020, "gray", 2000, 9099);
+        cars[1] = new Car(2, "BMW", "M4", 2018, "green", 4000, 4040);
+        cars[2] = new Car(3, "BMW", "7", 2015, "black", 2500, 1251);
+        cars[3] = new Car(4, "Audi", "A5", 2010, "red", 1900, 3371);
+        cars[4] = new Car(5, "Audi", "s3", 2021, "pink", 3500, 1101);
+        cars[5] = new Car(6, "Toyota", "Land Cruiser", 2009, "white", 3000, 3312);
+
+        return cars;
+    }
+
+    static Car[] cars_list() {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Введите количество автомобилей: ");
+        Car[] cars = new Car[sc1.nextInt()];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car();
+            System.out.println();
+            System.out.print("Введите id: ");
+            cars[i].setId(sc1.nextInt());
+            System.out.print("Введите марку: ");
+            cars[i].setMark(sc1.next());
+            System.out.print("Введите модель: ");
+            cars[i].setModel(sc1.next());
+            System.out.print("Введите год выпуска: ");
+            cars[i].setYear(sc1.nextInt());
+            System.out.print("Введите цвет: ");
+            cars[i].setColor(sc1.next());
+            System.out.print("Введите цену: ");
+            cars[i].setPrice(sc1.nextInt());
+            System.out.print("Введите регистрационный номер: ");
+            cars[i].setRegistration_number(sc1.nextInt());
+        }
+        return cars;
+    }
+
+    static void markCheck(Car[] cars, String mark) {
+        for (Car car : cars) {
+            if (car.getMark().equals(mark))
+                System.out.println(car.toString());
+        }
+        System.out.println();
+    }
+
+    static void modelCheck(Car[] cars, String model, int n) {
+        for (Car car : cars) {
+            if (car.getModel().equals(model) && (2022 - car.getYear()) > n) {
+                System.out.println(car.toString());
+            }
+        }
+        System.out.println();
+    }
+
+    static void yearCheck(Car[] cars, int year, int price) {
+        for (Car car : cars) {
+            if (car.getYear() == year && car.getPrice() > price)
+                System.out.println(car.toString());
+        }
     }
 }
+/*
+Создать классы, спецификации которых приведены ниже. Определить кон-
+структоры и методы set(), get(), toString(). Определить дополнительно
+методы в классе, создающем массив объектов. Задать критерий выбора данных
+и вывести эти данные на консоль. В каждом классе, обладающем информацией,
+должно быть объявлено несколько конструкторов.
+Car: id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер.
+Создать массив объектов. Вывести:
+a) список автомобилей заданной марки;
+b) список автомобилей заданной модели, которые эксплуатируются боль-
+ше n лет;
+c) список автомобилей заданного года выпуска, цена которых больше ука-
+занной.
+ */
